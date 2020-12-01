9edbecd79c4c0fa59c2720dc3966f33a2065df21
Sefa <sefa@Taners-MacBook-Pro.local>
2020-12-01 18:19:46 +0300

    nprogress color

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import Router from "next/router";
 import NProgress from "nprogress";
-import "nprogress/nprogress.css"; //styles of nprogress
-//Binding events.
+import "nprogress/nprogress.css";
 import "../src/global/styles.css";
+
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
